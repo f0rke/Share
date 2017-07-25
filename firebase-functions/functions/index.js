482c9766bc15e71caab63b5cae6dfee981a4b365
Moritz Köchig <M.Koechig@gmx.de>
2017-07-25 22:45:27 +0200

    - firebase functions

--- firebase-functions/functions/index.js
+++ firebase-functions/functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.channelChange = functions.database
+    .ref('/items')
+    .onWrite(event => {
+        var value = event.data.val()
+        console.log(value)
+        return
+    });
+
+exports.helloLisa = functions.https.onRequest((request, response) => {
+    var todayte = new Date();
+    var minutes = todayte.getMinutes();
+    var hours = todayte.getHours();
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    hours = (hours + 2) % 24
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    todayte = hours + ":" + minutes;
+    response.send("Hallo Lisa. Es ist " + todayte + " Uhr");
+});
